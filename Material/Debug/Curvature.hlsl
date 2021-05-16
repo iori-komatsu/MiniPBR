02dde30c0a756f8555c799c20b1a264625a07db2
Iori Komatsu <iori.komatsu@gmail.com>
2021-05-17 02:14:43 +0900

    曲率の単位を[1/mm]にする

--- Material/Debug/Curvature.hlsl
+++ Material/Debug/Curvature.hlsl
@@ -7,7 +7,8 @@ float2 Curvature(float3 worldPos, float3 normal) {
     float3 dNdy = ddy(normal);
     float cx = length(dNdx) / length(dPdx);
     float cy = length(dNdy) / length(dPdy);
-    return float2(cx, cy);
+    // 1/80 という係数は長さの尺度をMMD単位からミリメートルに直すために付けている。
+    return (1.0/80.0) * float2(cx, cy);
 }
 
 float3 ShaderSurface(
@@ -20,5 +21,5 @@ float3 ShaderSurface(
     uniform bool selfShadow
 ) {
     float2 c = Curvature(worldPos, normal);
-    return 0.2 * float3(c.x, c.y, 0);
+    return float3(c.x, c.y, 0);
 }
