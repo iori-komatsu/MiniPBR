ffab0012ca442aefda8f923cc55d34fdde0103d8
Iori Komatsu <iori.komatsu@gmail.com>
2021-05-18 22:42:49 +0900

    コントローラー対応

--- Material/Skin.hlsl
+++ Material/Skin.hlsl
@@ -3,7 +3,15 @@
 #include <Shader/ShadowMap/Sampling.fxsub>
 #include <Shader/Curvature.fxsub>
 
-const static float SubsurfaceScatteringFactor = 5.0;
+float _SpecularSmoothness        : CONTROLOBJECT < string name="SkinController.pmx"; string item="”½ŽËŒõ‚ÌŠŠ‚ç‚©‚³"; >;
+float _DiffuseSmoothness         : CONTROLOBJECT < string name="SkinController.pmx"; string item="ŠgŽUŒõ‚ÌŠŠ‚ç‚©‚³"; >;
+float _ReduceShadow              : CONTROLOBJECT < string name="SkinController.pmx"; string item="‰e‚ð”–‚ß‚é"; >;
+float _SubsurfaceScatteringScale : CONTROLOBJECT < string name="SkinController.pmx"; string item="”ç‰ºŽU—"; >;
+
+static const float SpecularRoughness = 1.0 - _SpecularSmoothness;
+static const float DiffuseRoughness  = 1.0 - _DiffuseSmoothness;
+static const float MinLightVisibility = _ReduceShadow;
+static const float SubsurfaceScatteringScale = lerp(1.0, 10.0, _SubsurfaceScatteringScale);
 
 texture2D PreIntegratedSkinLUT <
     string ResourceName = "../Misc/PreIntegratedSkin/LUT_Linear.png";
@@ -19,7 +27,7 @@ sampler2D PreIntegratedSkinLUTSamp = sampler_state {
 
 float3 SubsurfaceScattering(float dotNL, float3 worldPos, float3 normal) {
     float u = dotNL * 0.5 + 0.5;
-    float v = 1.0 - saturate(Curvature(worldPos, normal) * SubsurfaceScatteringFactor);
+    float v = 1.0 - saturate(Curvature(worldPos, normal) * SubsurfaceScatteringScale);
     return tex2D(PreIntegratedSkinLUTSamp, float2(u, v)).rgb;
 }
 
@@ -32,10 +40,10 @@ float3 ShaderSurface(
     float3 lightIrradiance,
     uniform bool selfShadow
 ) {
-    const float minLightVisibility = 0.3;
     float lightVisibility;
     if (selfShadow) {
-        lightVisibility = lerp(minLightVisibility, 1.0, CastShadow(worldPos, dot(normal, lightDir)));
+        float s = CastShadow(worldPos, dot(normal, lightDir));
+        lightVisibility = lerp(MinLightVisibility, 1.0, s);
     } else {
         lightVisibility = 1;
     }
@@ -46,11 +54,8 @@ float3 ShaderSurface(
     float dotNH = saturate(dot(normal, h));
     float dotVH = saturate(dot(viewDir, h));
 
-    const float specularRoughness = 0.35;
-    const float diffuseRoughness = 0.9;
     const float f0 = 0.04;
-
-    float3 fSpecular = SpecularBRDF(dotNL, dotNV, dotNH, dotVH, specularRoughness, f0);
+    float3 fSpecular = SpecularBRDF(dotNL, dotNV, dotNH, dotVH, SpecularRoughness, f0);
 
     float3 scatterCoeff = SubsurfaceScattering(dot(normal, lightDir), worldPos, normal);
     float3 fDiffuse = DiffuseBRDF(
@@ -59,7 +64,7 @@ float3 ShaderSurface(
         dot(lightDir, viewDir),
         dot(lightDir, h),
         baseColor,
-        diffuseRoughness) * scatterCoeff;
+        DiffuseRoughness) * scatterCoeff;
 
     return fSpecular * lightIrradiance * lightVisibility
          + fDiffuse * lightIrradiance * lightVisibility
