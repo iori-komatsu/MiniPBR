93a7eaa17be8da04dc8e4c3707efc6ab999d890d
Iori Komatsu <iori.komatsu@gmail.com>
2021-05-22 23:39:19 +0900

    NormalMapping実装

--- Material/Skin.hlsl
+++ Material/Skin.hlsl
@@ -1,2 +1,9 @@
 #define CONTROLLER_NAME "SkinController.pmx"
+
+#define PRE_INTEGRATED_SKIN_LUT_PATH "Texture/PreIntegratedSkinLUT.png"
+
+#define USE_NORMAL_MAP
+#define NORMAL_MAP_PATH "Texture/NM-Skin01.png"
+static const float NormalMapRepeat = 5;
+
 #include <Shader/Material/PreIntegratedSkin.fxsub>
