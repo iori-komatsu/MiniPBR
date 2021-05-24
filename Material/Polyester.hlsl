82029d61e63175829fd9b23d9080bc6426ce4e01
Iori Komatsu <iori.komatsu@gmail.com>
2021-05-24 23:52:15 +0900

    Polyester 追加

---
+++ Material/Polyester.hlsl
@@ -0,0 +1,10 @@
+static const float ReduceShadow = 0.1;
+static const float Roughness = 0.4;
+static const float F0 = 0.04;
+
+#define USE_NORMAL_MAP
+// https://3dtextures.me/2020/02/11/fabric-polyester-002/
+#define NORMAL_MAP_PATH "Texture/Fabric_Polyester_002_normal.jpg"
+static const float NormalMapRepeat = 5;
+
+#include <Shader/Material/Standard.fxsub>
--- Material/Standard.hlsl
+++ Material/Standard.hlsl
@@ -1,37 +1,5 @@
-#include <Shader/Material/Base.fxsub>
-#include <Shader/BRDF/Lambert.fxsub>
-#include <Shader/BRDF/Specular.fxsub>
-#include <Shader/ShadowMap/Sampling.fxsub>
+static const float ReduceShadow = 0.1;
+static const float Roughness = 0.35;
+static const float F0 = 0.04;
 
-float3 ShaderSurface(
-    float3 worldPos,
-    float3 baseColor,
-    float3 normal,
-    float3 viewDir,
-    float3 lightDir,
-    float3 lightIrradiance,
-    uniform bool selfShadow
-) {
-    const float minLightVisibility = 0.3;
-    float lightVisibility;
-    if (selfShadow) {
-        lightVisibility = lerp(minLightVisibility, 1.0, CastShadow(worldPos, dot(normal, lightDir)));
-    } else {
-        lightVisibility = 1;
-    }
-
-    float3 h = normalize(viewDir + lightDir);
-    float dotNL = dot(normal, lightDir);
-    float dotNV = saturate(dot(normal, viewDir)); // NÅEV < 0 ÇÕåvéZåÎç∑Ç≈ÇµÇ©Ç†ÇËÇ¶Ç»Ç¢ÇÃÇ≈ saturate Ç∑ÇÈ
-    float dotNH = dot(normal, h);
-    float dotVH = dot(viewDir, h);
-
-    const float specularRoughness = 0.35;
-    const float diffuseRoughness = 0.85;
-    const float f0 = 0.04;
-    float3 fSpecular = SpecularBRDF(dotNL, dotNV, dotNH, dotVH, specularRoughness, f0);
-    float3 fDiffuse = LambertBRDF(baseColor);
-
-    return (fSpecular + fDiffuse) * saturate(dotNL) * lightIrradiance * lightVisibility
-         + AmbientIrradiance * baseColor;
-}
+#include <Shader/Material/Standard.fxsub>
