9af0e840a7594ecee481fc9c17d2c36e5b6e6534
Iori Komatsu <iori.komatsu@gmail.com>
2021-05-24 22:54:11 +0900

    BRDFのファイルを分割

--- Material/Standard.hlsl
+++ Material/Standard.hlsl
@@ -1,5 +1,6 @@
 #include <Shader/Material/Base.fxsub>
-#include <Shader/BRDF.fxsub>
+#include <Shader/BRDF/Lambert.fxsub>
+#include <Shader/BRDF/Specular.fxsub>
 #include <Shader/ShadowMap/Sampling.fxsub>
 
 float3 ShaderSurface(
@@ -23,15 +24,13 @@ float3 ShaderSurface(
     float dotNL = dot(normal, lightDir);
     float dotNV = saturate(dot(normal, viewDir)); // NÅEV < 0 ÇÕåvéZåÎç∑Ç≈ÇµÇ©Ç†ÇËÇ¶Ç»Ç¢ÇÃÇ≈ saturate Ç∑ÇÈ
     float dotNH = dot(normal, h);
-    float dotLH = dot(lightDir, h);
     float dotVH = dot(viewDir, h);
-    float dotLV = dot(lightDir, viewDir);
 
     const float specularRoughness = 0.35;
     const float diffuseRoughness = 0.85;
     const float f0 = 0.04;
     float3 fSpecular = SpecularBRDF(dotNL, dotNV, dotNH, dotVH, specularRoughness, f0);
-    float3 fDiffuse = DiffuseBRDF(dotNL, dotNV, dotLV, dotLH, baseColor, diffuseRoughness);
+    float3 fDiffuse = LambertBRDF(baseColor);
 
     return (fSpecular + fDiffuse) * saturate(dotNL) * lightIrradiance * lightVisibility
          + AmbientIrradiance * baseColor;
