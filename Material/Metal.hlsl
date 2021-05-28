d990af6bb16df639e46f6d0a59d7820e4e16a4f1
Iori Komatsu <iori.komatsu@gmail.com>
2021-05-29 02:16:41 +0900

    Metal 追加

---
+++ Material/Metal.hlsl
@@ -0,0 +1,6 @@
+static const float ReduceShadow = 0.0;
+static const float Roughness = 0.45;
+static const float Metallic = 1.0;
+static const float Reflectance = 0.0;
+
+#include <Shader/Material/Standard.fxsub>
