aacc9a17eae35833c53aa3ef7e16a0229a74abcd
Iori Komatsu <iori.komatsu@gmail.com>
2021-05-29 02:09:26 +0900

    Standard Material に Metallic を追加

--- Material/Standard.hlsl
+++ Material/Standard.hlsl
@@ -1,5 +1,6 @@
 static const float ReduceShadow = 0.1;
 static const float Roughness = 0.35;
-static const float F0 = 0.04;
+static const float Metallic = 0.0;
+static const float Reflectance = 0.5;
 
 #include <Shader/Material/Standard.fxsub>
